25b7dc0a53c92c070b36e8aef94ea000d2e1f0e0
Ruben van Rijen <rubenvanrijen@gmail.com>
2023-10-24 23:16:29 +0200

    added php and renamed the index.css to style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./app/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,php}", "./app/**/*.{html,js,php}"],
   theme: {
     extend: {},
   },
